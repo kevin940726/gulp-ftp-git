f55138da187b61e93d2e2397f519dd962e53381c
kevin940726 <kevin830726@gmail.com>
2015-09-18 18:03:24 +0800

    add 'upload' task

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,40 @@ var gulp = require('gulp'),
     argv = require('yargs').argv, // pass arguments
     fs = require('fs'); // load file
 
+// function to upload to ftp server
+function upload(list) {
+    var conn = ftp.create({
+        host:     'your.host.name', // ftp host name
+        user:     'username', // ftp username
+        password: 'password', // ftp password
+        parallel: 10,
+        log:      gutil.log
+    });
+    var remotePath = '/path/to/remote'; // the remote path on the server you want to upload to
+
+    // added and modified files
+    var changes = list.reduce(function(a, cur) {
+        if (cur.type !== 'D' && cur.type.length)
+            a.push(cur.path);
+        return a || [];
+    }, []);
+    // deleted files
+    var deletes = list.reduce(function(a, cur) {
+        if (cur.type === 'D' && cur.type.length)
+            a.push(cur.path);
+        return a || [];
+    }, []);
+
+    // upload added and modified files
+    gulp.src(changes, { base: '.', buffer: false })
+        .pipe(conn.dest(remotePath));
+    // delete removes files
+    deletes.map(function(d) {
+        conn.delete(remotePath + d, function(err) {
+            if (err) throw err;
+        });
+    });
+}
 
 gulp.task('push', function() {
     // the remote name, default 'origin'
@@ -34,37 +68,14 @@ gulp.task('push', function() {
         return git.push(remote, branch, function(err) {
             if (err) throw err;
 
-            var conn = ftp.create({
-                host:     'your.host.name', // ftp host name
-                user:     'username', // ftp username
-                password: 'password', // ftp password
-                parallel: 10,
-                log:      gutil.log
-            });
-            var remotePath = '/path/to/remote'; // the remote path on the server you want to upload to
-
-            // added and modified files
-            var changes = list.reduce(function(a, cur) {
-                if (cur.type !== 'D' && cur.type.length)
-                    a.push(cur.path);
-                return a || [];
-            }, []);
-            // deleted files
-            var deletes = list.reduce(function(a, cur) {
-                if (cur.type === 'D' && cur.type.length)
-                    a.push(cur.path);
-                return a || [];
-            }, []);
-
-            // upload added and modified files
-            gulp.src(changes, { base: '.', buffer: false })
-                .pipe(conn.dest(remotePath));
-            // delete removes files
-            deletes.map(function(d) {
-                conn.delete(remotePath + d, function(err) {
-                    if (err) throw err;
-                });
-            });
+            upload(list);
         });
     });
+});
+
+
+gulp.task('upload', function() {
+    var list = JSON.parse(fs.readFileSync('.gulp-ftp-git-cache.json'));
+
+    return upload(list);
 });
